9742e23159407f93d55f129677caf8ef2484c4d4
Mathias Hansen <me@codemonkey.io>
2018-02-15 21:42:42 -0500

    Bumped geocod.io API version to v1.2

--- lib/geocodio/client.rb
+++ lib/geocodio/client.rb
@@ -18,7 +18,7 @@ module Geocodio
       :delete => Net::HTTP::Delete
     }
     HOST = 'api.geocod.io'
-    BASE_PATH = '/v1'
+    BASE_PATH = '/v1.2'
     PORT = 80
 
     def initialize(api_key = ENV['GEOCODIO_API_KEY'])
